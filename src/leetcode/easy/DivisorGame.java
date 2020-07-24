6459d6df1c26067095dbcca8a02b6eb0f48bd670
土豆卤蛋 <tudouludan@tudouludandeMacBook-Pro.local>
2020-07-24 09:13:08 +0800

    [leetcode]1025. 除数博弈

---
+++ src/leetcode/easy/DivisorGame.java
@@ -0,0 +1,21 @@
+package leetcode.easy;
+
+/**
+ * @description: 1025. 除数博弈
+ * @author: zexho
+ * @created: 2020/07/24 08:51
+ */
+public class DivisorGame {
+    public boolean divisorGame(int N) {
+        return fun(N,false);
+    }
+
+    public boolean fun(int N,boolean b){
+        for(int i = 1 ; i < N ; i++){
+            if(N%i==0){
+                return fun(N-i,!b);
+            }
+        }
+        return b;
+    }
+}
