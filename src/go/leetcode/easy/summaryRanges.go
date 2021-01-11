7eae0a4a66195085cfe8dab261db13764022abf9
zouzhihao-994 <zouzhihao994@gmail.com>
2021-01-11 20:46:21 +0800

    leetcode: 228 汇总区间
    Signed-off-by: zouzhihao-994 <zouzhihao994@gmail.com>

--- src/go/leetcode/easy/summaryRanges.go
+++ src/go/leetcode/easy/summaryRanges.go
@@ -1,1 +1,27 @@
 package easy
+
+import "strconv"
+
+func summaryRanges(nums []int) []string {
+	var ans []string
+	if len(nums) == 0 {
+		return ans
+	}
+	arrow := func(s, e int) {
+		if s == e {
+			ans = append(ans, strconv.Itoa(s))
+			return
+		}
+		ans = append(ans, strconv.Itoa(s)+"->"+strconv.Itoa(e))
+	}
+	l := len(nums)
+	s, e := 0, 1
+	for ; e < l; e++ {
+		if nums[e]-nums[e-1] > 1 {
+			arrow(nums[s], nums[e-1])
+			s = e
+		}
+	}
+	arrow(nums[s], nums[e-1])
+	return ans
+}
