3ead693c0f9a3ba20b5ee3bedc8ced8352fd8e78
zouzhihao-994 <zouzhihao994@gmail.com>
2021-02-16 08:36:57 +0800

    leetcode 561. 数组拆分
    Signed-off-by: zouzhihao-994 <zouzhihao994@gmail.com>

---
+++ src/go/leetcode/easy/arrayPairSum.go
@@ -0,0 +1,13 @@
+package easy
+
+import "sort"
+
+// 561. 数组拆分 I
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	ans := 0
+	for i := 0; i < len(nums); i += 2 {
+		ans += nums[i]
+	}
+	return ans
+}
