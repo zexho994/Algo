405a03f8e7bf3342c83dfec56b5dfac8f9ae71ec
zouzhihao-994 <zouzhihao994@gmail.com>
2021-02-21 10:42:30 +0800

    leetcode 239. 滑动窗口最大值
    Signed-off-by: zouzhihao-994 <zouzhihao994@gmail.com>

--- src/go/leetcode/hard/maxSlidingWindow.go
+++ src/go/leetcode/hard/maxSlidingWindow.go
@@ -1,27 +1,29 @@
 package hard
 
-// 单调队列
+// 滑动窗口 + 单调队列
 // 新加入的数如果比队尾的数大，那么弹出队尾的数，重复此步骤直到队列为空或者小于队尾的数
 func maxSlidingWindow(nums []int, k int) []int {
-	var queue, ans []int
-	length := len(nums)
-	push := func(i int) {
-		// 新加入的数替换更老且小的数
-		for len(queue) != 0 && nums[queue[len(queue)-1]] <= nums[i] {
-			queue = queue[:len(queue)-1]
+	var ansArr, decQueue []int
+	left := 0
+	push := func(n int) {
+		for len(decQueue) > 0 &&
+			n > decQueue[len(decQueue)-1] {
+			decQueue = decQueue[:len(decQueue)-1]
 		}
-		queue = append(queue, i)
+		decQueue = append(decQueue, n)
 	}
-	for i := 0; i < k; i++ {
-		push(i)
+	// 构建单调队列
+	for i := 0; i < k-1; i++ {
+		push(nums[i])
 	}
-	ans = append(ans, nums[queue[0]])
-	for i := k; i < length; i++ {
-		for len(queue) > 0 && queue[0] < i-k+1 {
-			queue = queue[1:]
+	// 统计结果
+	for i := k - 1; i < len(nums); i++ {
+		push(nums[i])
+		ansArr = append(ansArr, decQueue[0])
+		if nums[left] == decQueue[0] {
+			decQueue = decQueue[1:]
 		}
-		push(i)
-		ans = append(ans, nums[queue[0]])
+		left++
 	}
-	return ans
+	return ansArr
 }
