3accf0b7e2cb5af0cbeb360cfc80117be6c88ca9
zouzhihao-994 <zouzhihao994@gmail.com>
2021-03-25 23:58:57 +0800

    leetcode 82. 删除排序链表中的重复元素 II

--- src/go/linked/deleteDuplicates.go
+++ src/go/linked/deleteDuplicates.go
@@ -19,3 +19,25 @@ func deleteDuplicates(head *_struct.ListNode) *_struct.ListNode {
 	}
 	return h.Next
 }
+
+func deleteDuplicates1(head *ListNode) *ListNode {
+    if head == nil {
+        return nil
+    }
+
+    dummy := &ListNode{0, head}
+
+    cur := dummy
+    for cur.Next != nil && cur.Next.Next != nil {
+        if cur.Next.Val == cur.Next.Next.Val {
+            x := cur.Next.Val
+            for cur.Next != nil && cur.Next.Val == x {
+                cur.Next = cur.Next.Next
+            }
+        } else {
+            cur = cur.Next
+        }
+    }
+
+    return dummy.Next
+}
