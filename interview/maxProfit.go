59db303aae1c721243dc03276cf9f87fc7acebd0
zouzhihao-994 <zouzhihao994@gmail.com>
2021-04-09 23:12:41 +0800

    maxProfit 2
    Signed-off-by: zouzhihao-994 <zouzhihao994@gmail.com>

---
+++ interview/maxProfit.go
@@ -0,0 +1,14 @@
+package interview
+
+func maxProfit(prices []int) int {
+	if prices == nil {
+		return 0
+	}
+	profit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			profit += prices[i] - prices[i-1]
+		}
+	}
+	return profit
+}
