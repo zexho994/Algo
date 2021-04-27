d4b61c587bf8a05576ee116e531c13d6698d5bab
zexho994 <zouzhiaho994@gmail.com>
2021-04-27 11:10:17 +0800

    combine

---
+++ recall/combine_test.go
@@ -0,0 +1,28 @@
+package recall
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_combine(t *testing.T) {
+	type args struct {
+		n int
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+		{name: "t1", args: args{5, 3}, want: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := combine(tt.args.n, tt.args.k); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("combine() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
