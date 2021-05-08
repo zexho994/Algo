2adeb9678870b56765a8d7b5af9beebf81e76632
zexho994 <zouzhihao994@gmail.com>
2021-05-08 09:07:26 +0800

    75. 颜色分类 https://leetcode-cn.com/problems/sort-colors/

---
+++ daily-topic/sortColors_test.go
@@ -0,0 +1,21 @@
+package daily_topic
+
+import "testing"
+
+func Test_sortColors(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{name: "t1", args: args{nums: []int{2, 1}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sortColors(tt.args.nums)
+		})
+	}
+}
