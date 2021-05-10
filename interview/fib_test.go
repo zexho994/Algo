96c50074f4b408127ef735c81d0a57ab16ed0ed9
zexho994 <zouzhihao994@gmail.com>
2021-05-10 13:33:56 +0800

    剑指 Offer 10- I. 斐波那契数列 https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/

---
+++ interview/fib_test.go
@@ -0,0 +1,24 @@
+package interview
+
+import "testing"
+
+func Test_fib(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{name: "t1", args: args{n: 10}, want: 55},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fib(tt.args.n); got != tt.want {
+				t.Errorf("fib() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
