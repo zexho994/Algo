08fa710ef76ad40cd1d3261d15b0303458650a84
zexho994 <zouzhihao994@gmail.com>
2021-10-27 16:41:51 +0800

    [专项练习] 03 前n个数的二进制1的个数

---
+++ algo-java/src/专项突破/整数/CountBits.java
@@ -0,0 +1,20 @@
+package 专项突破.整数;
+
+/**
+ * @author Zexho
+ * @date 2021/10/27 4:20 下午
+ */
+public class CountBits {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        int mark = 1, idx = 1;
+        while (idx <= n) {
+            ans[idx] = ans[idx - mark] + 1;
+            idx++;
+            if (idx == mark * 2) {
+                mark = idx;
+            }
+        }
+        return ans;
+    }
+}
