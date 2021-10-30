8bd194607a26a42bd117363feef411603d6cee34
zexho994 <zouzhihao994@gmail.com>
2021-10-30 09:15:13 +0800

    [专项练习] 04 只出现一次的数，方法二

--- algo-java/src/专项突破/整数/SingleNumber.java
+++ algo-java/src/专项突破/整数/SingleNumber.java
@@ -16,4 +16,31 @@ public class SingleNumber {
         Arrays.stream(nums).forEach(n -> memo.put(n, memo.getOrDefault(n, 0) + 1));
         return memo.entrySet().stream().filter(e -> e.getValue() == 1).findFirst().get().getValue();
     }
+
+    //方法二：位个数计算
+    // [7 7 7 5]
+    // 5 -> 101
+    // 7 -> 111
+    // (434) / 3 = 101
+    public int singleNumber2(int[] nums) {
+        // 计算各个位的个数
+        // [3,1,4]
+        int[] arr = new int[32];
+        for (int num : nums) {
+            int n = num;
+            for (int j = 0; j < 32 && n != 0; j++) {
+                arr[j] += (n & 1);
+                n >>= 1;
+            }
+        }
+
+        // 转换成整数
+        int ans = 0, n = 1;
+        for (int i = 0; i < 32; i++) {
+            ans += n * (arr[i] % 3);
+            n *= 2;
+        }
+
+        return ans;
+    }
 }
