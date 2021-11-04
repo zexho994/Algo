997cf11f868563bd07ffa57d1f4c90aa0b204cc0
zexho994 <zouzhihao994@gmail.com>
2021-11-04 10:51:02 +0800

    [专项练习_字符串] 14. 字符串的变位词(双指针实现)

--- algo-java/src/专项突破/字符串/CheckInclusion.java
+++ algo-java/src/专项突破/字符串/CheckInclusion.java
@@ -1,5 +1,6 @@
 package 专项突破.字符串;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,10 @@ import java.util.Map;
  * @date 2021/11/3 7:45 下午
  */
 public class CheckInclusion {
+
+    /**
+     * 方法一: 滑动窗口
+     */
     public boolean checkInclusion(String s1, String s2) {
         Map<Character, Integer> map = new HashMap<>(s1.length());
         for (char c : s1.toCharArray()) {
@@ -38,4 +43,36 @@ public class CheckInclusion {
         }
         return false;
     }
+
+    /**
+     * 方法二 : 双指针
+     */
+    public boolean checkInclusion2(String s1, String s2) {
+        if(s1.length() > s2.length()){
+            return false;
+        }
+        int[] arr = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            arr[s1.charAt(i) - 'a']++;
+            arr[s2.charAt(i) - 'a']--;
+        }
+        if (isAllZero(arr)) {
+            return true;
+        }
+        int left = 0, right = s1.length();
+        while (right < s2.length()) {
+            arr[s2.charAt(left++) - 'a']++;
+            arr[s2.charAt(right++) - 'a']--;
+            if (isAllZero(arr)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isAllZero(int[] arr) {
+        return Arrays.stream(arr).allMatch(e -> e == 0);
+    }
+
+
 }
