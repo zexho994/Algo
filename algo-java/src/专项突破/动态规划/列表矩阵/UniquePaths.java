075c01aa2e437af945b45c7d492cd1945fbb10a1
zexho994 <zouzhihao994@gmail.com>
2021-11-24 08:56:49 +0800

    [专项练习_DP] 98 路径的数目（mn空间复杂度）

---
+++ algo-java/src/专项突破/动态规划/列表矩阵/UniquePaths.java
@@ -0,0 +1,19 @@
+package 专项突破.动态规划.列表矩阵;
+
+/**
+ * @author Zexho
+ * @date 2021/11/24 8:48 上午
+ */
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] arr = new int[m + 1][n + 1];
+        arr[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j + 1] += arr[i][j];
+                arr[i + 1][j] += arr[i][j];
+            }
+        }
+        return arr[m - 1][n - 1];
+    }
+}
