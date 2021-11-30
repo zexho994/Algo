b90a9013421028d29e31b3983b014dccbe643391
zexho994 <zouzhihao994@gmail.com>
2021-11-30 18:52:50 +0800

    [专项练习_DP_背包问题]<103> 最少硬币数（一维数组，迭代解法,简洁版）

--- algo-java/src/专项突破/动态规划/背包问题/CoinChange.java
+++ algo-java/src/专项突破/动态规划/背包问题/CoinChange.java
@@ -6,19 +6,15 @@ package 专项突破.动态规划.背包问题;
  */
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
-        }
         int[] arr = new int[amount + 1];
         for (int i = 1; i < arr.length; i++) {
+            arr[i] = amount + 1;
             for (int coin : coins) {
-                if (i == coin) {
-                    arr[i] = 1;
-                } else if (i > coin && arr[i - coin] > 0) {
-                    arr[i] = Math.min(arr[i] == 0 ? Integer.MAX_VALUE : arr[i], arr[i - coin] + 1);
+                if (i >= coin) {
+                    arr[i] = Math.min(arr[i], arr[i - coin] + 1);
                 }
             }
         }
-        return arr[amount] == 0 ? -1 : arr[amount];
+        return arr[amount] > amount ? -1 : arr[amount];
     }
 }
