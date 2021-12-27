52df55b02a6035f832265958e839d6b1ad6e44fc
zexho994 <zouzhihao994@gmail.com>
2021-12-27 10:41:20 +0800

    [专项练习]<树> 53 二叉搜索树的中序后继（使用boolean）

--- algo-java/src/专项突破/树/二叉搜索树/InorderSuccessor.java
+++ algo-java/src/专项突破/树/二叉搜索树/InorderSuccessor.java
@@ -2,35 +2,29 @@ package 专项突破.树.二叉搜索树;
 
 import struct.TreeNode.TreeNode;
 
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * @author Zexho
  * @date 2021/12/24 2:53 PM
  */
 public class InorderSuccessor {
 
-    List<TreeNode> list = new LinkedList<>();
+    TreeNode pre = null;
+    boolean flag;
 
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        inorder(root);
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i).val == p.val) {
-                return list.get(i + 1);
-            } else if (list.get(i).val > p.val) {
-                return list.get(i);
+        if (root == null) {
+            return null;
+        }
+        if (!flag) {
+            inorderSuccessor(root.left, p);
+        }
+        if (!flag) {
+            if (pre != null && pre.val >= p.val) {
+                flag = true;
             }
+            pre = root;
+            inorderSuccessor(root.right, p);
         }
-        return null;
+        return flag ? pre : null;
     }
-
-    private void inorder(TreeNode node) {
-        if (node != null) {
-            inorder(node.left);
-            list.add(node);
-            inorder(node.right);
-        }
-    }
-
 }
