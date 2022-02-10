869d5e36ad1ec58855b7361bd669df030dcc41d1
zexho994 <zouzhihao994@gmail.com>
2022-02-10 11:26:25 +0800

    [程序员面试经典系列] 01. 判断字符是否唯一, 使用数位

--- 精选200/src/IsUnique.java
+++ 精选200/src/IsUnique.java
@@ -17,4 +17,19 @@ public class IsUnique {
         return true;
     }
 
+    // 方法2：使用数位记录字符,int类型有32位，可以装下26个字符。
+    // 例如：
+    // a = 1 << 0
+    // d = 1 << 4
+    // 总结 ==> X = 1 << (字母表的顺序位置)
+    public boolean isUnique2(String str) {
+        int check = 0;
+        for (Character ch : str.toCharArray()) {
+            if ((check & (1 << (ch - 'a'))) > 0) {
+                return false;
+            }
+            check |= (1 << (ch - 'a'));
+        }
+        return true;
+    }
 }
---
+++ 精选200/src/IsUniqueTest.java
@@ -0,0 +1,7 @@
+class IsUniqueTest {
+    public static void main(String[] args) {
+        IsUnique isUnique = new IsUnique();
+        boolean abc = isUnique.isUnique2("abc");
+        assert abc;
+    }
+}
