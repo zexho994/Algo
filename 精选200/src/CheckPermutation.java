67b918cd5ee115673dd9bbfb03420cda7530857f
zexho994 <zouzhihao994@gmail.com>
2022-02-11 09:55:02 +0800

    [程序员面试经典系列] 02.判定是否互为重排序,使用数组记录

--- 精选200/src/CheckPermutation.java
+++ 精选200/src/CheckPermutation.java
@@ -6,7 +6,12 @@ import java.util.Map;
  * @date 2022/2/10 11:34 AM
  */
 public class CheckPermutation {
+
+    // 使用map记录
     public boolean checkPermutation(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
         Map<Character, Integer> map = new HashMap<>(32);
         for (Character s : s1.toCharArray()) {
             if (!map.containsKey(s)) {
@@ -15,18 +20,30 @@ public class CheckPermutation {
                 map.compute(s, (k, v) -> v + 1);
             }
         }
-
         for (Character s : s2.toCharArray()) {
-            if (!map.containsKey(s)) {
+            if (!map.containsKey(s) || map.get(s) == 0) {
                 return false;
             }
-            if (map.get(s) == 1) {
-                map.remove(s);
-            } else {
-                map.compute(s, (k, v) -> v - 1);
+            map.compute(s, (k, v) -> v - 1);
+        }
+        return true;
+    }
+
+    // 使用数组记录
+    public boolean checkPermutation2(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] arr = new int[26];
+        for (Character c : s1.toCharArray()) {
+            arr[c - 'a']++;
+        }
+        for (Character c : s2.toCharArray()) {
+            arr[c - 'a']--;
+            if (arr[c - 'a'] < 0) {
+                return false;
             }
         }
-
-        return map.size() == 0;
+        return true;
     }
 }
