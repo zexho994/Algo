442a1345bcdc0a7843583e3e456c0e7bcce3ae8d
zexho994 <zouzhihao994@gmail.com>
2022-03-27 20:22:17 +0800

    全排列

---
+++ feishu/PermuteTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+class PermuteTest {
+
+    @Test
+    void permute() {
+        Permute permute = new Permute();
+        permute.permute(new int[]{1, 2, 3});
+    }
+}
