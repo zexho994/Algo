80eb610114872101c5642d5e7b997a90499fd50e
zexho994 <zouzhihao994@gmail.com>
2022-05-23 22:59:49 +0800

    refactor IsValid

--- host100/IsValid.java
+++ host100/IsValid.java
@@ -3,22 +3,22 @@ import java.util.Stack;
 /**
  * @author : zexho
  * @created : 2022-05-19
-**/
-public class IsVaild{
+ **/
+public class IsValid {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch == '(' || ch == '[' || ch == '{'){
+            if (ch == '(' || ch == '[' || ch == '{') {
                 stack.push(ch);
-            }else if (ch == ')'){
-                if(stack.isEmpty() || stack.peek() != '(') return false;
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') return false;
                 stack.pop();
-            }else if (ch == ']'){
-                if(stack.isEmpty() || stack.peek() != '[') return false;
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') return false;
                 stack.pop();
-            }else {
-                if(stack.isEmpty() || stack.peek() != '{') return false;
+            } else {
+                if (stack.isEmpty() || stack.peek() != '{') return false;
                 stack.pop();
             }
         }
