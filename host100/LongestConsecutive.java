3d19d56b6cf0bf65460854c68c33599c74737e7c
zexho994 <zouzhihao994@gmail.com>
2022-05-29 19:54:46 +0800

    验证二叉搜索树

---
+++ host100/LongestConsecutive.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author : zexho
+ * @created : 2022-05-29
+**/
+public class LongestConsecutive{
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        for(int n : nums){
+            set.add(n);
+        }
+        int maxLen = 0;
+        for(int i = 0; i < nums.length; i++){
+            int len = 1; 
+            int n = nums[i];
+            if(set.contains(n-1)){
+                continue;
+            }
+            while(set.contains(++n)){
+                len++;
+            }
+            maxLen = Math.max(len,maxLen);
+        }
+        return maxLen;
+    }
+}
