75f545cbfde5fe646273c462d6f83bd2713b8a45
zexho994 <zouzhihao994@gmail.com>
2022-08-30 20:22:03 +0800

    [程序员面试经典] 面试题 01.09 字符串轮转,解法二
    Signed-off-by: zexho994 <zouzhihao994@gmail.com>

--- 2022/08/IsFlipedString.java
+++ 2022/08/IsFlipedString.java
@@ -14,4 +14,9 @@ public class IsFlipedString {
         }
         return false;
     }
+
+    public boolean isFlipedString2(String s1, String s2) {
+        return s1.length() == s2.length() && (s1 + s1).contains(s2);
+    }
+
 }
--- 2022/08/Partition.java
+++ 2022/08/Partition.java
@@ -1,25 +1,24 @@
-import struct.ListNode.ListNode;
 
 public class Partition {
 
-	public ListNode partition(ListNode head, int x) {
-		ListNode small = new ListNode(0);
-		ListNode great = new ListNode(0);
-		ListNode s = small;
-		ListNode g = great;
-		while (head != null) {
-			if (head.val < x) {
-				small.next = head;
-				small = small.next;
-			} else {
-				great.next = head;
-				great = great.next;
-			}
-			head = head.next;
-		}
-		great.next = null;
-		small.next = g.next;
-		return s;
-	}
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode great = new ListNode(0);
+        ListNode s = small;
+        ListNode g = great;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                great.next = head;
+                great = great.next;
+            }
+            head = head.next;
+        }
+        great.next = null;
+        small.next = g.next;
+        return s;
+    }
 
 }
