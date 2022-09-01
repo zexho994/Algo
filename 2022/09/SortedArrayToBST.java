918b8ef8d65d1602b61276c7bffe41fe37fd9a11
zexho994 <zouzhihao994@gmail.com>
2022-09-01 12:19:54 +0800

    [程序员面试经典] 面试题 04.02. 最小高度树
    Signed-off-by: zexho994 <zouzhihao994@gmail.com>

---
+++ 2022/09/SortedArrayToBST.java
@@ -0,0 +1,15 @@
+
+public class SortedArrayToBST {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode build(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = (left + right) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = build(nums, left, mid - 1);
+        node.right = build(nums, mid + 1, right);
+        return node;
+    }
+}
