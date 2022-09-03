73f1311f2e1b96c43c8be9b5fadc026094cb12c9
zexhozou <zouzhihao994@gmail.com>
2022-09-03 18:26:59 +0800

    [程序员面试经典]面试题 04.06. 后继者

--- 2022/09/InorderSuccessor.java
+++ 2022/09/InorderSuccessor.java
@@ -1,6 +1,13 @@
 public class InorderSuccessor {
     TreeNode node = null;
 
+    /**
+     * 通过递归的方式遍历
+     *
+     * @param root
+     * @param p
+     * @return
+     */
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         if (root == null || node != null) return node;
 
@@ -13,5 +20,26 @@ public class InorderSuccessor {
             inorderSuccessor(root.right, p);
         }
         return node;
+    }
+
+    /**
+     * 通过迭代的方式遍历
+     *
+     * @param root
+     * @param p
+     * @return
+     */
+    public TreeNode inorderSuccessor2(TreeNode root, TreeNode p) {
+        TreeNode cur = root;
+        TreeNode succ = null;
+        while (cur != null) {
+            if (p.val < cur.val) {
+                succ = cur;
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        return succ;
     }
 }
